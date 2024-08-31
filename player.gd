62782428244ef635262a0aeb4144733d705a25ef
Lhaaits <l.haaitsma@gmail.com>
2024-08-31 22:55:30 +0200

    first commit

--- player.gd
+++ player.gd
@@ -4,6 +4,13 @@ extends RigidBody2D
 @export var fart_scene: PackedScene
 var fart_frame: Vector2
 
+
+func _process(delta):
+	var screen_size = get_viewport_rect().size
+	if position.y > screen_size.y + 300 || position.y < -400:
+		position.y = screen_size.y/2
+		set_linear_velocity(Vector2(0,0))
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -17,7 +24,6 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("fart"):
-		print_debug("farted")
 		set_linear_velocity(Vector2.UP * up_factor)
 		var fart: AnimatedSprite2D = fart_scene.instantiate()
 		fart.position   = get_node("Kitty/Butt").position + Vector2(40, -40)
